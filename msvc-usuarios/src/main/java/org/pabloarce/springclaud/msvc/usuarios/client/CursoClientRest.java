61f490fed637fc89921ec1bc81fb6f2477c9b198
Pablo-Arce-Jemersoft <parce@jemersoft.com>
2023-09-25 10:18:15 -0300

    feature: docker file de msvc cursos, previo a comunicación entre contenedores

--- msvc-usuarios/src/main/java/org/pabloarce/springclaud/msvc/usuarios/client/CursoClientRest.java
+++ msvc-usuarios/src/main/java/org/pabloarce/springclaud/msvc/usuarios/client/CursoClientRest.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "msvc-cursos", url = "localhost:8002")
+@FeignClient(name = "msvc-cursos", url = "msvc-cursos:8002")
 public interface CursoClientRest {
     @DeleteMapping("/eliminar-usuario-curso/{id}")
     void eliminarUsuarioCursoPorId(@PathVariable Long id);
